8e84b311f0c2a55b3ba3936346bf3b744cdaf52c
sturead_chy <1149959299@qq.com>
2019-09-17 00:00:33 +0800

    第一次提交代码

---
+++ src/main/java/com/line/controller/HelloController.java
@@ -0,0 +1,20 @@
+package com.line.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+/**
+ * @program demo
+ * @description:
+ * @author: chenyang
+ * @create: 2019/09/16 17:22
+ */
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String sayHello(){
+        return "hello demo";
+    }
+}
